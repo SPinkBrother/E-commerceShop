b721144148633dc05bdd3d1927f73573a2ea2962
NguyenHongAnh <spinkbrotherofficial@gmail.com>
2025-08-22 10:41:37 +0700

    Fixing user icon logic

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ migrate = Migrate(app, db)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+# User profile route
+@app.route("/<int:user_id>_profile")
+def profile(user_id):
+    return render_template("profile.html",user_id= user_id)
+
 # Cart route
 @app.route("/mycart")
 def mycart():
